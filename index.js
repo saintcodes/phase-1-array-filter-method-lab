952f4ae3a3b19186b9dae60ebd1d401977119b7e
Michelle <clairsin@gmail.com>
2022-04-28 12:10:01 -0400

    commit tree 5a616adb1240fb0ea816d8742acd2bcf286465d2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, name) {
+  let result = array.filter(element => element.toLowerCase() === name.toLowerCase()
+  )
+  return result
+}
+
+function fuzzyMatch(array, string) {
+  let result = array.filter(element => element.toLowerCase().indexOf(string.toLowerCase()) === 0)
+  return result
+}
+
+function matchName(obj, string) {
+  let result = obj.filter(element => element.name === string)
+  return result
+}
